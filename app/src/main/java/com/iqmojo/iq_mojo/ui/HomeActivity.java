cbdcf3d2c20a1294021aa599c8ff4458eb6b6188
Shubham Lamba <Shubham.Lamba@myoxigen.com>
2017-08-18 18:11:27 +0530

    gradient bg

--- app/src/main/java/com/iqmojo/iq_mojo/ui/HomeActivity.java
+++ app/src/main/java/com/iqmojo/iq_mojo/ui/HomeActivity.java
@@ -1,19 +1,65 @@
 package com.iqmojo.iq_mojo.ui;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.ViewDragHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.iqmojo.R;
 
+import nl.psdcompany.duonavigationdrawer.views.DuoDrawerLayout;
+import nl.psdcompany.duonavigationdrawer.widgets.DuoDrawerToggle;
 
-public class HomeActivity extends AppCompatActivity {
+
+public class HomeActivity extends AppCompatActivity implements DrawerLayout.DrawerListener {
+
+    FrameLayout container;
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        // Home Activity
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        container = (FrameLayout) findViewById(R.id.container);
+        setSupportActionBar(mToolbar);
+        DuoDrawerLayout drawerLayout = (DuoDrawerLayout) findViewById(R.id.drawer);
+        DuoDrawerToggle drawerToggle = new DuoDrawerToggle(this, drawerLayout, mToolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+
+        drawerLayout.setDrawerListener(this);
+        drawerToggle.syncState();
+
+
+
+    }
+
+    @Override
+    public void onDrawerSlide(View drawerView, float slideOffset) {
+
+    }
+
+    @Override
+    public void onDrawerOpened(View drawerView) {
+
+        container.setElevation(30f);
+    }
+
+    @Override
+    public void onDrawerClosed(View drawerView) {
+
+        container.setElevation(10f);
+    }
+
+    @Override
+    public void onDrawerStateChanged(int newState) {
+
 
     }
 }
