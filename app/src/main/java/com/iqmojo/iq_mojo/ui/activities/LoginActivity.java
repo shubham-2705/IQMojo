65633f75572c64da753f4371562a1a921a53daf8
himanshu <him20488@gmail.com>
2017-08-19 23:38:23 +0530

    login activity

---
+++ app/src/main/java/com/iqmojo/iq_mojo/ui/activities/LoginActivity.java
@@ -0,0 +1,106 @@
+package com.iqmojo.iq_mojo.ui.activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.iqmojo.R;
+import com.iqmojo.base.ui.activity.BaseActivity;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
+/**
+ * Created by himanshu on 19/8/17.
+ */
+
+public class LoginActivity extends BaseActivity implements View.OnClickListener {
+
+    Toolbar mToolbar;
+    RelativeLayout RlFbLogin, RlsGoogleLogin;
+    CallbackManager callbackManager;
+    Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        context = LoginActivity.this;
+        callbackManager = CallbackManager.Factory.create();
+        RlFbLogin = (RelativeLayout) findViewById(R.id.rlyFb);
+        RlFbLogin.setOnClickListener(this);
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+
+                        Toast.makeText(context, "succ", Toast.LENGTH_SHORT).show();
+
+
+                        GraphRequest request = GraphRequest.newMeRequest(
+                                loginResult.getAccessToken(),
+                                new GraphRequest.GraphJSONObjectCallback() {
+                                    @Override
+                                    public void onCompleted(JSONObject object, GraphResponse response) {
+                                        Log.v("LoginActivity", response.toString());
+
+
+                                    }
+                                });
+
+                        Bundle parameters = new Bundle();
+                        parameters.putString("fields", "id, first_name, last_name, email, gender, birthday, location");
+                        request.setParameters(parameters);
+                        request.executeAsync();
+
+
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()){
+
+            case R.id.rlyFb:
+                LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
