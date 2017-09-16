7aa2ad71bd05857a509b1851be079632706e5d2d
himanshu <him20488@gmail.com>
2017-09-17 03:14:48 +0530

    paytm and bank payment screens

---
+++ app/src/main/java/com/iqmojo/iq_mojo/ui/activities/PaytmActivity.java
@@ -0,0 +1,16 @@
+package com.iqmojo.iq_mojo.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.iqmojo.R;
+import com.iqmojo.base.ui.activity.BaseActivity;
+
+public class PaytmActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paytm);
+    }
+}
